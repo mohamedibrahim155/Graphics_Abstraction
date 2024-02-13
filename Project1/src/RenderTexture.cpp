94b3cd10adff7675c63c0f56aa3d4510bbbdb47d
mohamedibrahim155 <63718196+mohamedibrahim155@users.noreply.github.com>
2024-02-12 23:23:19 -0500

    render texture class added

--- Project1/src/RenderTexture.cpp
+++ Project1/src/RenderTexture.cpp
@@ -1,1 +1,24 @@
 #include "RenderTexture.h"
+
+void RenderTexture::Bind()
+{
+	GLCALL(glBindTexture(GL_TEXTURE_2D, framebuffer->GetRendererID()));
+}
+
+void RenderTexture::Unbind()
+{
+	GLCALL(glBindTexture(GL_TEXTURE_2D, 0));
+}
+
+void RenderTexture::SetTextureSlot(int place)
+{	
+	GLCALL(glActiveTexture(GL_TEXTURE0 + place));
+}
+
+void RenderTexture::DrawProperties()
+{
+}
+
+void RenderTexture::SceneDraw()
+{
+}
--- Project1/src/RenderTexture.h
+++ Project1/src/RenderTexture.h
@@ -1,5 +1,19 @@
 #pragma once
-class RenderTexture
+#include "BaseTexture.h"
+#include "FrameBuffer.h"
+class RenderTexture : public BaseTexture
 {
+public:
+
+	RenderTexture() = default;
+
+	// Inherited via BaseTexture
+	void Bind() override;
+	void Unbind() override;
+	void SetTextureSlot(int place = 0) override;
+	void DrawProperties() override;
+	void SceneDraw() override;
+
+	FrameBuffer* framebuffer = nullptr;
 };
 
